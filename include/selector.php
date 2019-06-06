b5ddfd4717810640d0fac8af84fbce0c2639a8ba
tacching <51411107+tacching@users.noreply.github.com>
2019-06-06 10:28:40 +0900

    update image

--- include/selector.php
+++ include/selector.php
@@ -49,9 +49,9 @@ if ( empty($_SESSION['SELECTOR']) ) {
         $ini_array = parse_ini_file($ini_file);     
         $_SESSION['SELECTOR'] = $ini_array['selector'];
     } elseif ( $_SESSION['V2'] == 'true' )  {
-        $_SESSION['SELECTOR'] = 'cats';
+        $_SESSION['SELECTOR'] = 'cities';
     } else {
-        $_SESSION['SELECTOR'] = 'cats';
+        $_SESSION['SELECTOR'] = 'cities';
     }
 
 }                                     
